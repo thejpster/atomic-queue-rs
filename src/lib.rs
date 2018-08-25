bdbcd4ac6ee752a55677f7243ac378948e6c0097
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2018-08-25 19:08:47 +0100

    Push supports concurrent writes. Added is_full and is_empty.

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ where
     data: UnsafeCell<[T; 4]>,
     read: AtomicUsize,
     write: AtomicUsize,
+    written: AtomicUsize,
 }
 
 impl<T> FixedQueue4<T>
@@ -28,24 +29,69 @@ where
             data: UnsafeCell::new([default; 4]),
             read: ATOMIC_USIZE_INIT,
             write: ATOMIC_USIZE_INIT,
+            written: ATOMIC_USIZE_INIT,
         }
+    }
+
+    fn counter_to_idx(&self, counter: usize) -> usize {
+        counter % Self::QUEUE_LEN
+    }
+
+    pub fn is_full(&self) -> bool {
+        let write_counter = self.write.load(Ordering::SeqCst);
+        let read_counter = self.read.load(Ordering::SeqCst);
+        (write_counter.wrapping_sub(read_counter)) >= Self::QUEUE_LEN
+    }
+
+    pub fn is_empty(&self) -> bool {
+        let written_counter = self.written.load(Ordering::SeqCst);
+        let read_counter = self.read.load(Ordering::SeqCst);
+        written_counter == read_counter
     }
 
     /// Add an item to the queue. An error is returned if the queue is full.
     pub fn push(&self, value: T) -> Result<(), ()> {
-        let wp = self.write.load(Ordering::SeqCst);
-        let rp = self.read.load(Ordering::SeqCst);
-        if (wp - rp) < Self::QUEUE_LEN {
-            let write_idx = self.write.load(Ordering::SeqCst) % Self::QUEUE_LEN;
-            unsafe {
-                let p = &mut *self.data.get();
-                p[write_idx] = value;
+        // Loop until we've allocated ourselves some space without colliding
+        // with another writer.
+        let idx = loop {
+            let write_counter = self.write.load(Ordering::SeqCst);
+            let read_counter = self.read.load(Ordering::SeqCst);
+            if (write_counter.wrapping_sub(read_counter)) >= Self::QUEUE_LEN {
+                // Queue is full - quit now
+                return Err(());
             }
-            self.write.fetch_add(1, Ordering::SeqCst);
-            Ok(())
-        } else {
-            Err(())
+            if self.write.compare_and_swap(
+                write_counter,
+                write_counter.wrapping_add(1),
+                Ordering::SeqCst,
+            ) == write_counter
+            {
+                // We've managed increment `self.write` successfully without
+                // anyone else updating it underneath us.
+                break self.counter_to_idx(write_counter);
+            }
+        };
+        // This is safe because we're the only possible thread with this value
+        // of idx (reading or writing).
+        let p = unsafe { &mut *self.data.get() };
+        p[idx] = value;
+
+        // Now update `self.written` so that readers can read what we just wrote.
+        loop {
+            let written_counter = self.written.load(Ordering::SeqCst);
+            if self.written.compare_and_swap(
+                written_counter,
+                written_counter.wrapping_add(1),
+                Ordering::SeqCst,
+            ) == written_counter
+            {
+                // We've managed increment `self.written` successfully without
+                // anyone else updating it underneath us.
+                break;
+            }
         }
+
+        Ok(())
     }
 
     /// Look at the next item in the queue, but do not remove it.
@@ -87,26 +133,38 @@ mod test {
     fn create_queue() {
         let q = FixedQueue4::new(0u32);
         assert!(q.push(1).is_ok());
+        assert!(!q.is_full());
         assert!(q.push(2).is_ok());
+        assert!(!q.is_full());
         assert!(q.push(3).is_ok());
+        assert!(!q.is_full());
         assert_eq!(q.peek(), Some(1));
         assert_eq!(q.pop(), Some(1));
         assert_eq!(q.pop(), Some(2));
         assert_eq!(q.pop(), Some(3));
         assert_eq!(q.pop(), None);
+        assert!(q.is_empty());
         assert!(q.push(4).is_ok());
+        assert!(!q.is_empty());
         assert_eq!(q.pop(), Some(4));
+        assert!(q.is_empty());
         assert_eq!(q.pop(), None);
+        assert!(q.is_empty());
     }
 
     #[test]
     fn overflow_queue() {
         let q = FixedQueue4::new(0u32);
         assert!(q.push(1).is_ok());
+        assert!(!q.is_full());
         assert!(q.push(2).is_ok());
+        assert!(!q.is_full());
         assert!(q.push(3).is_ok());
+        assert!(!q.is_full());
         assert!(q.push(4).is_ok());
+        assert!(q.is_full());
         assert!(q.push(5).is_err());
+        assert!(q.is_full());
         assert_eq!(q.pop(), Some(1));
         assert_eq!(q.pop(), Some(2));
         assert_eq!(q.pop(), Some(3));
