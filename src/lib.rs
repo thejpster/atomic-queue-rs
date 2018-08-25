a5dfe24e53b18e96b6d2a20e52a6757a71e3d44a
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2018-08-25 20:17:51 +0100

    Add some docs and licensing.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+//! # Atomic Queue
+//!
+//! This is a simple lock-free atomic queue for `#![no_std]` embedded systems
+//! written in Rust. The queue can be any length because the storage is
+//! supplied as a mutable slice at creation time.
+//!
+//! ```rust
+//! extern crate atomic_queue;
+//! #[macro_use]
+//! extern crate lazy_static;
+//!
+//! use atomic_queue::AtomicQueue;
+//!
+//! /// This is the static storage we use to back our queue
+//! static mut STORAGE: [u8; 16] = [0; 16];
+//! lazy_static! {
+//!     static ref QUEUE: AtomicQueue<'static, u8> = {
+//!         let m = unsafe { AtomicQueue::new(&mut STORAGE) };
+//!         m
+//!     };
+//! }
+//!
+//! fn main() -> Result<(), ()> {
+//!     println!("Pushed 255");
+//!     QUEUE.push(255)?;
+//!     println!("Popped {:?}", QUEUE.pop());
+//!     Ok(())
+//! }
+//! ```
+
 #![no_std]
 
 #[macro_use]
